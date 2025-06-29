645837465ce8aabd5078cf61986d01331d316704
PeeyushDas <122cs0071@nitrkl.ac.in>
2025-06-29 14:32:36 +0530

    fix: title and favicon

--- portfolio/lib/main.dart
+++ portfolio/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Peeyush\'s Portfolio',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'nasalization',
